704302e8739749df6c12a312353b439ad16ad836
Sean La <seanla1996@gmail.com>
2016-05-20 17:51:06 -0600

    Added a few changes to dp

--- dp/dp.hpp
+++ dp/dp.hpp
@@ -1,5 +1,3 @@
-#include <string>
-
 class OptimalAlignment
 {
 	public:
--- dp/test.cpp
+++ dp/test.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-#include <limits>
 #include "dp.hpp"
 
 int main()
 {
-	std::string uLR = "ggggatcAAAAgtttt"; // uncorrected long read
+	std::string uLR = "ggggatcgtttt"; // uncorrected long read
 	std::string cLR = "ggggatcgtttt"; // corrected long read
 
 	OptimalAlignment alignment(uLR, cLR);
