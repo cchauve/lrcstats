35dd13b7c6d100c88b0556595695dce5589c131f
Sean La <seanla1996@gmail.com>
2016-05-27 12:22:45 -0600

    Removed some files

--- other/retrieve_fastq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while IFS='' read -r line || [[ -n "$line" ]]; do
-	echo "Beginning retrieval of file $line"
-	/global/scratch/seanla/sratoolkit.2.6.2-ubuntu64/bin/fastq-dump --outdir /global/scratch/seanla/Data/peru/ --gzip $line
-	echo "Finished retrieving file $line"	
-done < "$1"
