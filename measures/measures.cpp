4438be9f5cb547eb03c7a1cfcbdf70f8aa07b26c
Sean <seanla1996@gmai.com>
2016-05-29 14:05:58 -0700

    Removed corrected segments, edit distance function and added documentation

--- measures/measures.cpp
+++ measures/measures.cpp
@@ -2,26 +2,29 @@
 #include <string>
 #include <algorithm>
 
-int editScore(std::string ref, std::string uLR)
+int editScore(std::string ref, std::string lr)
 {
 /* Since maf files give the true alignment, we can find the true "edit distance"
- * (or edit score, as we call it) without trying to find an approximation. */
+ * (or edit score, as we call it) without trying to find an approximation.
+ * We can also use this module to find the edit distance between cLRs and the ref
+ * using the same metric as when calculating the edit score between the ref and lr
+ */
 	int score = 0;
 	int del = 1;
 	int ins = 1;
 	int sub = 1;
 	int length = ref.length();
 	char refBase;
-	char uBase;
+	char base;
 
 	for (int seqIndex = 0; seqIndex < length; seqIndex++) {
 		refBase = ref[seqIndex];
-		uBase = uLR[seqIndex];
+		base = lr[seqIndex];
 
-		if (refBase != uBase) {
+		if (refBase != base) {
 			if (refBase == '-') {
 				score = score + ins;
-			} else if (uBase == '-') {
+			} else if (base == '-') {
 				score = score + del;
 			} else {
 				score = score + sub;
@@ -30,56 +33,4 @@ int editScore(std::string ref, std::string uLR)
 	}		
 	return score;
 }
-
-int editDistance(std::string ref, std::string cLR)
-{
-/* Usual Levenshtein edit distance function */
-	int delCost = 1;
-	int insCost = 1;
-	int subCost = 1;
-	int rows = cLR.length() + 1;
-	int columns = ref.length() + 1;
-	int cIndex;
-	int refIndex;
-	int deletion;
-	int insertion;
-	int substitution;
-	int matrix[rows][columns];
-	char cBase;
-	char refBase;
-
-        for (int rowIndex = 0; rowIndex < rows; rowIndex++) {
-                matrix[rowIndex][0] = rowIndex;
-        }
-
-        for (int columnIndex = 1; columnIndex < columns; columnIndex++) {
-                matrix[0][columnIndex] = columnIndex;
-        }
-
-	for (int rowIndex = 1; rowIndex < rows; rowIndex++) {
-		for (int columnIndex = 1; columnIndex < columns; columnIndex++) {
-			cIndex = rowIndex - 1;
-			refIndex = columnIndex - 1;
-			cBase = toupper( cLR[cIndex] );
-			refBase = toupper( ref[refIndex] );
-
-			if (cBase == refBase) {
-				matrix[rowIndex][columnIndex] = matrix[rowIndex-1][columnIndex-1];				
-			} else {
-				deletion = matrix[rowIndex][columnIndex-1] + delCost;
-				insertion = matrix[rowIndex-1][columnIndex] + insCost; 
-				substitution = matrix[rowIndex-1][columnIndex-1] + subCost;
-				matrix[rowIndex][columnIndex] = std::min( deletion, std::min(insertion, substitution) ); 
-			}
-		}
-	}
-	return matrix[rows-1][columns-1];
-}
-
-
-
-
-
-
-
 
--- measures/measures.hpp
+++ measures/measures.hpp
@@ -1,3 +1,1 @@
 int editScore(std::string refAlign, std::string uLR_align);
-
-int editDistance(std::string ref, std::string cLR);
