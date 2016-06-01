92f42ea010876a2fefc2d6d345b3517c04105b0b
Sean La <seanla1996@gmail.com>
2016-06-01 09:00:15 -0600

    Added more comments to alignments header

--- alignments/alignments.hpp
+++ alignments/alignments.hpp
@@ -5,26 +5,31 @@ class Alignments
 {
         public:
                 Alignments(std::string reference, std::string uLongRead, std::string cLongRead);
+		Alignments (const Alignments &alignments);
                 ~Alignments();
                 std::string getClr();
                 std::string getUlr();
                 std::string getRef();
                 std::string get_cAlignment();
                 std::string getRefAlignment();
-                int getDistance();
         private:
+		// long reads and ref before processing
                 std::string clr;
                 std::string ulr;
                 std::string ref;
+		// Returns long reads and ref in format suitable for MAF file
                 std::string clrMaf;
                 std::string ulrMaf;
                 std::string refMaf;
+		// Similar to Maf format, but deletes pairs of the form (-,-)
                 std::string cAlignment;
                 std::string refAlignment;
+		// Specs for matrix
                 int rows;
                 int columns;
+		// Holds matrix
                 int** matrix;
-                int distance;
+		// Member functions
                 void printMatrix();
                 int cost(char refBase, char cBase);
                 void findAlignments();
