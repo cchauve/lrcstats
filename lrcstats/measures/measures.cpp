590123c1fbb41099aa6c5b45793b94ee2c99c880
Sean La <seanla1996@gmail.com>
2016-06-30 16:10:20 -0600

    Added some statistics functions

--- lrcstats/measures/measures.cpp
+++ lrcstats/measures/measures.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <cassert> // for assert
+#include <cctype> // for toupper, tolower, isupper, and islower
 #include "measures.hpp"
 
 int editScore(std::string ref, std::string lr)
@@ -35,3 +36,146 @@ int editScore(std::string ref, std::string lr)
 	return score;
 }
 
+int64_t substitutions(std::string ref, std::string read)
+// Returns the number of substitutions between the reference and read string
+{
+	assert( ref.length() == read.length() );
+
+	int64_t subs = 0;
+
+	for (int64_t index = 0; index < ref.length(); index++) {
+		if ( toupper(ref[index]) != toupper(read[index]) ) {
+			subs++;
+		}			
+	}
+
+	return subs;
+}
+
+int64_t insertions(std::string ref, std::string read)
+// Returns the number of insertions between the reference and read string
+{
+	assert( ref.length() == read.length() );
+
+	int64_t ins = 0;
+
+	for (int64_t index = 0; index < ref.length(); index++) {
+		if (ref[index] == '-' and read[index] != '-') {
+			ins++;
+		}
+	}
+
+	return ins;
+}
+
+int64_t deletions(std::string ref, std::string read)
+// Returns the number of insertions between the reference and read string
+{
+	assert( ref.length() == read.length() );
+
+	int64_t del = 0;
+
+	for (int64_t index = 0; index < ref.length(); index++) {
+		if (ref[index] != '-' and read[index] == '-') {
+			del++;
+		}
+	}
+
+	return del;
+}
+
+int64_t correctedTruePositives(std::string ref, std::string read)
+// Returns the number of pairs of bases such that the read base is corrected and
+// the read base is equivalent to the reference base.
+{
+	assert( ref.length() == read.length() );
+
+	int64_t truePositives = 0;
+
+	for (int64_t index = 0; index < ref.length(); index++) {
+		if ( isupper(read[index]) and toupper(read[index]) == toupper(ref[index]) ) {
+			truePositives++;		
+		}	
+	}
+
+	return truePositives;
+}
+
+int64_t correctedFalsePositives(std::string ref, std::string read)
+// Returns the number of pairs of bases such that the read base is corrected and
+// the read base is not equivalent to the reference base.
+{
+	assert( ref.length() == read.length() );
+
+	int64_t falsePositives = 0;
+
+	for (int64_t index = 0; index < ref.length(); index++) {
+		if ( isupper(read[index]) and toupper(read[index]) != toupper(ref[index]) ) {
+			falsePositives++;		
+		}	
+	}
+
+	return falsePositives;
+}
+
+int64_t uncorrectedTruePositive(std::string ref, std::string read)
+// Returns the number of pairs of bases where the read base is uncorrected and
+// the read base is equivalent to the reference base.
+{
+	assert( ref.length() == read.length() );
+
+	int64_t truePositives = 0;
+
+	for (int64_t index = 0; index < ref.length(); index++) {
+		if ( islower(read[index]) and tolower(read[index]) == tolower(ref[index]) ) {
+			truePositives++;		
+		}	
+	}
+
+	return truePositives;
+}
+
+int64_t uncorrectedFalsePositives(std::string ref, std::string read)
+// Returns the number of pairs of bases where the read base is uncorrected and
+// the read base is not equivalent to the reference base.
+{
+	assert( ref.length() == read.length() );
+
+	int64_t falsePositives = 0;
+
+	for (int64_t index = 0; index < ref.length(); index++) {
+		if ( islower(read[index]) and tolower(read[index]) != tolower(ref[index]) ) {
+			falsePositives++;		
+		}	
+	}
+
+	return falsePositives;
+}
+
+int64_t correctedBases(std::string read)
+// Returns the number of corrected bases in the read
+{
+	assert( read.length() > 0 );
+
+	int64_t corrected = 0;
+
+	for (int64_t index = 0; index < read.length(); index++) { 
+		if ( isupper(read[index]) ) { corrected++; }	
+	}
+
+	return corrected;
+}
+
+int64_t uncorrectedBases(std::string read);
+// Returns the number of uncorrected bases in the read 
+{
+	assert( read.length() > 0 );
+	
+	int64_t uncorrected = 0;
+
+	for (int64_t index = 0; index < read.length(); index++) {
+		if ( islower(read[index]) ) { uncorrected++; } 
+	}
+
+	return uncorrected;
+}
--- lrcstats/measures/measures.hpp
+++ lrcstats/measures/measures.hpp
@@ -1,8 +1,41 @@
 #ifndef MEASURES_H
 #define MEASURES_H
 
-int editScore(std::string ref, std::string lr);
+#include <cstdint>
+
+int64_t editScore(std::string ref, std::string lr);
 /* Returns the "edit score" of two alignments. 
  * Uses a similar scoring schema as the Levenshtein edit distance equations. */
+
+int64_t substitutions(std::string ref, std::string read);
+// Returns the number of substitutions between the reference and read string
+
+int64_t insertions(std::string ref, std::string read);
+// Returns the number of insertions between the reference and read string
+
+int64_t deletions(std::string ref, std::string read);
+// Returns the number of insertions between the reference and read string
+
+int64_t correctTruePositives(std::string ref, std::string read);
+// Returns the number of pairs of bases such that the read base is corrected and
+// the read base is equivalent to the reference base.
+
+int64_t correctFalsePositives(std::string ref, std::string read);
+// Returns the number of pairs of bases such that the read base is corrected and
+// the read base is not equivalent to the reference base.
+
+int64_t uncorrectedTruePositive(std::string ref, std::string read);
+// Returns the number of pairs of bases where the read base is uncorrected and
+// the read base is equivalent to the reference base.
+
+int64_t uncorrectedFalsePositives(std::string ref, std::string read);
+// Returns the number of pairs of bases where the read base is uncorrected and
+// the read base is not equivalent to the reference base.
+
+int64_t correctedBases(std::string read);
+// Returns the number of corrected bases in the read
+
+int64_t uncorrectedBases(std::string read);
+// Returns the number of uncorrected bases in the read 
 
 #endif // MEASURES_H
