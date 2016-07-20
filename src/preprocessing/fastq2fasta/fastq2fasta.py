ebe965cf94c1bbd2b15f38c17f8f9d960d38a444
Sean La <laseanl@sfu.ca>
2016-07-20 11:07:45 -0700

    Deleted unneeded variable in fastq2fasta

--- src/preprocessing/fastq2fasta/fastq2fasta.py
+++ src/preprocessing/fastq2fasta/fastq2fasta.py
@@ -1,4 +1,4 @@
-import sys, getopt
+import os, sys, getopt
 
 helpMessage = "Convert FASTQ files to FASTA format."
 usageMessage = "Usage: %s [-h help and usage] [-i FASTQ file path] [-o output prefix]" % (sys.argv[0])
@@ -32,7 +32,6 @@ outputPath = "%s.fasta" % (outputPrefix)
 
 # Flag to determine if we're reading a new sample from the FASTQ file
 newSample = False
-sequence = ''
 
 with open(inputPath, 'r') as inputFile:
 	with open(outputPath, 'w') as outputFile:
@@ -41,7 +40,8 @@ with open(inputPath, 'r') as inputFile:
 			if len(line) > 0 and line[0] is '@':
 				newSample = True
 				header = ">%s" % (line[1:])
-			# This conditional only happens immediately after the sequence identifier line
+			# This conditional only happens immediately after reading
+			# the sequence identifier line
 			elif newSample:
 				outputFile.write(header)
 				outputFile.write(line)
