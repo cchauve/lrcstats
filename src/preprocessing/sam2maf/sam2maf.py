4d1c59b39bad9bffbd9f508874f4e9a15e85f81a
Sean La <seanla1996@gmail.com>
2016-08-12 14:39:07 -0600

    Added unit tests to sam2maf

--- src/preprocessing/sam2maf/sam2maf.py
+++ src/preprocessing/sam2maf/sam2maf.py
@@ -1,24 +1,38 @@
-import sys, getopt
+import sys
+import getopt
 
-def addSpaces(cigar):
+def getDelimitedCigar(cigar):
+	'''
+	Returns a space delimited cigar string.
+	e.g. "11M3D" -> "11 M 3 D"	
+	'''
 	newCigar = ""
-	lastWasAlpha = False 
 	for i in range( len(cigar) ):
+		# If the current char in the string is not a digit,
+		# then it is an operation, so we add a space around it.
 		if not cigar[i].isdigit():
 			newCigar = newCigar + " " + cigar[i] + " "
-			lastWasAlpha = True
 		else:
 			newCigar = newCigar + cigar[i] 
 	return newCigar
 
 def getCigarList(cigar):
+	'''
+	Returns a list of operations given a space delimited CIGAR string.
+	e.g. "3 M 3 D 3 I" -> ['M', 'M', 'M', 'D', 'D', 'D', 'I', 'I', 'I']
+	'''
 	cigar = cigar.split()
+
+	assert len(cigar) % 2 == 0
 
 	numOps = []
 	ops = []
+
 	for i in range( len(cigar) ):
+		# Chars at even indices of the CIGAR list are ints
 		if i % 2 == 0:
 			numOps.append( int(cigar[i]) )
+		# Odd are the CIGAR opts
 		else:
 			ops.append( cigar[i] )
 
@@ -26,25 +40,35 @@ def getCigarList(cigar):
 
 	cigarList = [] 
 	
+	# Converts these lists:
+	# numOps = [3, 3, 3]
+	# ops = ['M', 'D', 'I']
+	# Into:
+	# ['M', 'M', 'M', 'D', 'D', 'D', 'I', 'I', 'I']
 	for i in range( len(numOps) ):
 		for u in range( numOps[i] ):
 			cigarList.append(ops[i])	
 	return cigarList 
 
 def nextBase(base, flag):
+	'''
+	If flag is 16, then the sequence is question is the reverse
+	complement of the original sequence and we return the complement
+	of base. Otherwise, just return the original base.
+	'''
+	complement = { 'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G' }
+
 	if (flag == 16):
-		if base == "A":
-			return "T"
-		elif base == "T":
-			return "A"
-		elif base == "G":
-			return "C"
-		elif base == "C":
-			return "G"
+		return complement[base]
 	else:
 		return base
 
 def getRefSeq(ref, refPos, cigarList):
+	'''
+	Returns the segment of the reference sequence with starting base
+	at refPos and length equivalent to the number of non-'I' chars
+	in the cigarList.
+	'''
 	refSeq = ""
 	index = 0
 
@@ -55,11 +79,17 @@ def getRefSeq(ref, refPos, cigarList):
 	return refSeq
 
 def getReverse(seq):
+	'''
+	Returns the reverse of seq.
+	'''
 	return seq[::-1]
 
-def getRefAlignment(refSeq, refPos, cigarList, flag):
-
-	index = 0
+def getRefAlignment(refSeq, cigarList, flag):
+	'''
+	Given the refSeq, SAM flag and list of CIGAR ops cigarList,
+	returns the reference alignment to the long read.
+	'''
+	refIndex = 0
 	refAlignment = ""
 
 	for i in range( len(cigarList) ):
@@ -67,11 +97,17 @@ def getRefAlignment(refSeq, refPos, cigarList, flag):
 		if currentOp == "I":
 			refAlignment += "-"
 		else:
-			refAlignment += nextBase( refSeq[index], flag )
-			index += 1
+			refAlignment += nextBase( refSeq[refIndex], flag )
+			# We only increment the refIndex if the current cigar OP is not an
+			# insertion
+			refIndex += 1
 	return refAlignment
 
 def getReadAlignment(read, cigarList):
+	'''
+	Given the read and the CIGAR list, returns the alignment
+	of the read to the reference sequence.
+	'''
 	readAlignment = ""
 	index = 0
 
@@ -86,6 +122,9 @@ def getReadAlignment(read, cigarList):
 	return readAlignment
 
 def getReference(refPath):
+	'''
+	Retrieve the reference genome from refPath
+	'''
 	reference = ""
 	with open(refPath, 'r') as file:
 		for line in file:
@@ -96,6 +135,9 @@ def getReference(refPath):
 	return reference
 
 def getGaplessLength(seq):
+	'''
+	Returns the number of non-'-' bases in the seq
+	'''
 	length = 0
 	for i in range( len(seq) ):
 		if seq[i] != "-":
@@ -103,6 +145,9 @@ def getGaplessLength(seq):
 	return length
 
 def convert(ref, samPath, mafPath):
+	'''
+	Converts the SAM file into a MAF file.
+	'''
 	srcSize = len( ref )
 	readNumber = 0
 
@@ -112,14 +157,17 @@ def convert(ref, samPath, mafPath):
 				if len( line ) > 0 and line[0] != "@":
 					line = line.split("	")
 
+					# Get relevant info from the SAM file for that particular read
 					flag = int( line[1] )
 					start = int( line[3] ) - 1
 					cigar = line[5]
 					read = line[9]
 
-					cigar = addSpaces(cigar)
+					# Get the list of CIGAR ops
+					cigar = getDelimitedCigar(cigar)
 					cigarList = getCigarList(cigar)
 
+					# get the reference sequence from the reference genome
 					refSeq = getRefSeq(ref, start, cigarList)
 
 					if flag == 16:
@@ -145,13 +193,59 @@ def convert(ref, samPath, mafPath):
 					maf.write(readLine)
 					maf.write("\n")
 					readNumber += 1
-				
-						
 
-helpMessage = ""
-usageMessage = "-h -r refpath -s sam path -o maf path"
+def unitTest():
+	'''
+	Unit test for this script.
+	'''
+	print "Testing module..."
 
-options = "hr:s:o:"
+	# Test delimitCigar module
+	cigar = "10I1=1M2X3D"				
+	actualDelimitCigar = "10 I 1 = 1 M 2 X 3 D "						
+	delimitCigar = getDelimitedCigar(cigar) 
+
+	assert actualDelimitCigar == delimitCigar
+
+	actualCigarList = []
+
+	for i in range(10):
+		actualCigarList.append('I')
+
+	for i in range(1):
+		actualCigarList.append('=')
+
+	for i in range(1):
+		actualCigarList.append('M')
+
+	for i in range(2):
+		actualCigarList.append('X')
+
+	for i in range(3):
+		actualCigarList.append('D')
+
+	cigarList = getCigarList(delimitCigar) 
+
+	assert cigarList == actualCigarList
+
+	bases = ['A', 'C', 'G', 'T']
+	complements = ['T', 'G', 'C', 'A']	
+	flags = [0, 16]
+
+	for flag in flags:
+		for i in range( len(bases) ):
+			if flag == 0:
+				assert nextBase(bases[i], flag) == bases[i]
+			else:
+				assert nextBase(bases[i], flag) == complements[i]
+
+	print "All tests passed!"
+
+helpMessage = ("Converts SAM file to Multiple Alignment Format.\n"
+		+ "Behavior only defined for CIGAR ops 'D', '=', 'I', 'M', or 'X'")
+usageMessage = "[-h help and usage] [-r <path to the reference FASTA file>] [-s <path to the SAM file>] [-o <MAF output path>]"
+
+options = "hr:s:o:t"
 
 try:
 	opts, args = getopt.getopt(sys.argv[1:], options)
@@ -178,6 +272,9 @@ for opt, arg in opts:
 		samPath = arg
 	elif opt == '-o':
 		mafPrefix = arg
+	elif opt == '-t':
+		unitTest()
+		sys.exit()
 
 if refPath is None or samPath is None or mafPrefix is None:
 	print helpMessage
