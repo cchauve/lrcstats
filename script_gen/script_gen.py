c74712cfcdd162988a06d9a17eaa59a6988fe8ae
Sean La <laseanl@sfu.ca>
2016-08-19 11:31:42 -0700

    Moved readConfig method to separate file

--- script_gen/script_gen.py
+++ script_gen/script_gen.py
@@ -1,10 +1,12 @@
 import argparse
+import sys
+import read_config
 
 def createBlankConfig():
 	'''
 	Creates a blank configuration file in the current directory.
 	'''
-	config ="[paths]\n" \
+	config ="[variables]\n" \
 		"# For each path, don't include the leading \ please!\n" \
 		"\n" \
 		"# Absolute path to LRCStats dir\n" \
@@ -23,26 +25,20 @@ def createBlankConfig():
 		"jabba = \n" \
 		"\n" \
 		"# Directory to store read data\n" \
-		"data = \n"
+		"data = \n" \
+		"\n" \
+		"# File names of the real PacBio read FASTQ files for\n" \
+		"# E.coli, yeast and fly\n" \
+		"yeast_fastq = \n" \
+		"ecoli_fastq = \n" \
+		"fly_fastq = \n" \
+		"\n" \
+		"# Your email address to send PBS job info to\n" \
+		"email = \n"
+
 	blankConfigPath = "blank.config"
 	with open(blankConfigPath,'w') as file:
 		file.write(config)	
-
-def readConfig(configPath):
-	'''
-	Reads a configuration file and outputs a dict of paths
-	to the necessary programs.
-	''' 
-	with open(configPath, 'r') as config:
-		paths = {}
-		for line in config:
-			tokens = line.split()
-			# hashtags are comments
-			if line[0] != "#" and len(tokens) == 3:
-				key = tokens[0]
-				path = tokens[2]
-				paths[key] = path				
-	return paths
 
 MAJOR_VERSION = 1
 MINOR_VERSION = 0
@@ -53,15 +49,25 @@ parser = argparse.ArgumentParser(description='''
 	Version %d.%d
 	''' % (MAJOR_VERSION, MINOR_VERSION))
 
-parser.add_argument('-c', '--config', metavar='PATH', type=str, help="path to the configuration file")
 parser.add_argument('-b', '--blank_config', action='store_true', help=
 	"""
 	create a new blank configuration file in the current directory 
 	to construct your own testing pipeline and exit the program
 	""")
 
+requiredNamed = parser.add_argument_group('required named arguments')
+requiredNamed.add_argument('-c', '--config', metavar='CONFIG', type=str, help="path to the configuration file")
+
 args = parser.parse_args()
 
 if args.blank_config:
-	print("Creating a new configuration file..")
 	createBlankConfig()
+	print("Created a new blank configuration file.")
+	sys.exit()
+
+if args.config:
+	variables = read_config.readConfig( args.config )
+	print("Read configuration file.")
+else:
+	print("Error; please provide a configuration file.")
+	sys.exit(2) 
