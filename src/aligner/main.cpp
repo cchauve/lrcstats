d81e2890ddc07e22cf82fdb9ed7dffb4823abecf
Sean La <seanla1996@gmail.com>
2016-08-31 11:25:19 -0700

    modified main to perform alignment in current thread, too

--- src/aligner/main.cpp
+++ src/aligner/main.cpp
@@ -168,12 +168,16 @@ void generateTrimmedMaf()
 	std::cout << "Aligning " << ::g_threads << " partitions of reads concurrently...";
 	std::vector< std::future<std::vector<Read_t>> > partitionThread;
 	
-	for (int64_t i = 0; i < partitions.size(); i++) {
+	// Only create g_threads - 1 new threads; work will also be done on the main thread
+	for (int64_t i = 1; i < partitions.size(); i++) {
 		partitionThread.push_back( std::async( &alignTrimmedReads, partitions.at(i) ) );
 	} 
 
 	// Once the threads have finished doing their thing, get all the aligned partitions of reads
 	std::vector< std::vector<Read_t> > alignedPartitions;
+
+	// Perform work in this thread, too
+	alignedPartitions.push_back( alignTrimmedReads( partitions.at(0) ) );
 
 	for (int64_t i = 0; i < partitionThread.size(); i++) {
 		alignedPartitions.push_back( partitionThread.at(i).get() );
@@ -211,12 +215,15 @@ void generateUntrimmedMaf()
 	std::cout << "Aligning partitions of reads concurrently...";
 	std::vector< std::future<std::vector<Read_t>> > partitionThread;
 	
-	for (int64_t i = 0; i < partitions.size(); i++) {
+	for (int64_t i = 1; i < partitions.size(); i++) {
 		partitionThread.push_back( std::async( &alignUntrimmedReads, partitions.at(i) ) );
 	} 
 
 	// Once the threads have finished doing their thing, get all the aligned partitions of reads
 	std::vector< std::vector<Read_t> > alignedPartitions;
+
+	// Perform work in this thread, too
+	alignedPartitions.push_back( alignUntrimmedReads( partitions.at(0) ) );
 
 	for (int64_t i = 0; i < partitionThread.size(); i++) {
 		alignedPartitions.push_back( partitionThread.at(i).get() );
