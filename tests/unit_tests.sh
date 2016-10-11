b73fccaac3d79cd1ec35ab369380df4d1294bee7
Sean La <seanla1996@gmail.com>
2016-10-10 18:38:54 -0700

    fixed unit_tests script

--- tests/unit_tests.sh
+++ tests/unit_tests.sh
@@ -5,7 +5,7 @@ set -e
 echo "Performing unit tests..."
 
 # Perform unit tests for aligner
-cd ../src/aligner/unit_tests/
+cd src/aligner/unit_tests/
 make & ./unit_tests_aligner
 
 echo "Unit tests completed."
