1b3fd01e5f8f4a9ba43ad11c291ce451bca21c35
Sean La <seanla1996@gmail.com>
2016-10-10 19:32:47 -0700

    updated unti tests sh" ;

--- tests/unit_tests.sh
+++ tests/unit_tests.sh
@@ -6,6 +6,7 @@ echo "Performing unit tests..."
 
 # Perform unit tests for aligner
 cd src/aligner/unit_tests/
-make & ./unit_tests_aligner
+make
+./unit_tests_aligner
 
 echo "Unit tests completed."
