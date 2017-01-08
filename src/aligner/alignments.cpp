bc019dd734d9761a222ab920ee8d510cf8b338f5
Sean La <seanla1996@gmail.com>
2017-01-07 16:40:46 -0800

    updated debugging features in alignments

--- src/aligner/alignments.cpp
+++ src/aligner/alignments.cpp
@@ -350,7 +350,7 @@ void UntrimmedAlignments::findAlignments()
 						refMaf = ref[urIndex] + refMaf;
 						columnIndex--;
 					} else {
-						std::cout << "ERROR CODE 2: No paths found. Terminating backtracking.\n";
+						std::cout << "ERROR CODE 2: Terminating backtracking.\n";
 						std::exit(1);
 					}
 				}
@@ -375,7 +375,7 @@ void UntrimmedAlignments::findAlignments()
 						rowIndex--;
 						columnIndex--;
 					} else {
-						std::cout << "ERROR CODE 3: No paths found. Terminating backtracking.\n";
+						std::cout << "ERROR CODE 3: Terminating backtracking.\n";
 						std::exit(1);
 					}
 				} else if (ulr[urIndex] == '-') {
@@ -385,13 +385,25 @@ void UntrimmedAlignments::findAlignments()
 						refMaf = ref[urIndex] + refMaf;
 						columnIndex--;
 					} else {
-						std::cout << "ERROR CODE 4: No paths found. Terminating backtracking.\n";
+						std::cout << "ERROR CODE 4: Terminating backtracking.\n";
 						std::exit(1);
 					}
 				} else {
-					std::cout << "ERROR CODE 5: No paths found. Terminating backtracking.\n";
+					std::cout << "ERROR CODE 5: Terminating backtracking.\n";
+					if (cIndex > 0) {
+						std::cout << "cLR[i-1] = " << clr[cIndex-1] << std::endl;
+					}
+					if (urIndex > 0) {
+						std::cout << "uLR[j-1] = " << ulr[urIndex-1] << std::endl;
+					}
 					std::cout << "cLR[i] = " << clr[cIndex] << std::endl;
 					std::cout << "uLR[j] = " << ulr[urIndex] << std::endl;
+					if (cIndex < clr.length() - 1) {
+						std::cout << "cLR[i+1] = " << clr[cIndex+1] << std::endl;
+					}
+					if (urIndex < ulr.length() - 1) {
+						std::cout << "uLR[i+1] = " << ulr[urIndex+1] << std::endl;
+					}
 					std::exit(1);	
 				}
 		// This condition is performed if the current corrected long read base is uppercase
@@ -437,7 +449,7 @@ void UntrimmedAlignments::findAlignments()
 					rowIndex--;
 					columnIndex--;
 				} else {
-					std::cout << "ERROR CODE 6: No paths found. Terminating backtracking.\n";
+					std::cout << "ERROR CODE 6: Terminating backtracking.\n";
 					std::exit(1);	
 				}
 			} 		
@@ -633,7 +645,7 @@ void TrimmedAlignments::findAlignments()
 			rowIndex--;
 			columnIndex--;
 		} else {
-			std::cout << "ERROR: No paths found. Terminating backtracking.\n";
+			std::cout << "ERROR: Terminating backtracking.\n";
 			std::cout << "cIndex is " << cIndex << "\n";
 			std::cout << "urIndex is " << urIndex << "\n";
 			std::exit(1);
